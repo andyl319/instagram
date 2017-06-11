12bda720a340fc8306a324598021341cc86f0009
Andrew Lim <andyl319@yahoo.com>
2017-06-10 17:30:58 -0700

    add user controller, model, and session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,47 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :username, :session_token, uniqueness: true
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+  before_validation :ensure_session_token_uniqueness
+
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      return user
+    end
+    nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_session_token_uniqueness
+    while User.find_by(session_token: self.session_token)
+      self.session_token = SecureRandom.urlsafe_base64(16)
+    end
+  end
+
 end
