10c73d931c329430648973931b957fae034ea67c
Andrew Lim <andyl319@yahoo.com>
2017-06-12 01:09:12 -0700

    add index page styling

--- frontend/components/photo/photo_index_item.jsx
+++ frontend/components/photo/photo_index_item.jsx
@@ -6,10 +6,12 @@ const PhotoIndexItem = ({photo, router}) => {
     <Link className="photo-index-item"
       to={`/photos/${photo.id}`}>
       <li className="photo-index-detail">
-        <span className="photo-index-user">{photo.user.username}</span>
+        <span className="photo-index-info">
+          <span className="info-user"><span className="fa fa-user-circle" aria-hidden="true"></span>&nbsp;&nbsp;{photo.user.username}</span>
+          <span className="info"><span className="fa fa-location-arrow" aria-hidden="true"></span>&nbsp;&nbsp;{photo.location}</span>
+        </span>
         <img className="photo-index-art" src={photo.url} />
         <span className="photo-index-caption">{photo.caption}</span>
-        <span className="photo-index-location">{photo.location}</span>
       </li>
     </Link>
   );
