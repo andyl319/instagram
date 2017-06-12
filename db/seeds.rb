3b8c59c1c33970a24bdb706914f14098f55f6e4c
Andrew Lim <andyl319@yahoo.com>
2017-06-12 05:32:32 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,46 @@
 guest = User.create(username: 'guest', password: 'abc123')
 lh = User.create(username: 'LewisHamilton', password: 'password')
 pw = User.create(username: 'PabloWalker', password: 'password')
+
+one = Photo.create(
+  user_id: 1,
+  url: "https://res.cloudinary.com/dfkydaeys/image/upload/v1497250083/instagram/vjns3sizrejxhkrbooyi.jpg",
+  caption: "what moves you? #porsche #930 #explore",
+  location: "New York, NY"
+)
+
+two = Photo.create(
+  user_id: 1,
+  url: "https://res.cloudinary.com/dfkydaeys/image/upload/v1497266268/instagram/chjckfqpor3m4rdf0zqr.jpg",
+  caption: "The Man, The Myth, The Legend. #MagnusWalker #Porsche #901",
+  location: "Los Angelos, Ca"
+)
+
+three = Photo.create(
+  user_id: 2,
+  url: "https://res.cloudinary.com/dfkydaeys/image/upload/v1497267747/instagram/rveqwxnbludviv73zpym.jpg",
+  caption: "1 of 5!!! Thanks Pagani #TeamAMG #TeamPagani",
+  location: "Rome, Italy"
+)
+
+four = Photo.create(
+  user_id: 2,
+  url: "https://res.cloudinary.com/dfkydaeys/image/upload/v1497268237/instagram/usydhbwt5ey9zf8emdc6.jpg",
+  caption: "I live for this moment #TeamAMG #TeamPagani ",
+  location: "Yas Island Marina Circuit, Abu Dhabi",
+)
+
+five = Photo.create(
+  user_id: 3,
+  url: "https://res.cloudinary.com/dfkydaeys/image/upload/v1497268696/instagram/axyg8eh3iegqt9aftcpm.jpg",
+  caption:
+   "Sometimes the hurdles aren't really hurdles at all. They're welcome challenges, tests.",
+  location: "Needville, Tx"
+)
+
+six = Photo.create(
+  user_id: 3,
+  url: "https://res.cloudinary.com/dfkydaeys/image/upload/v1497269088/instagram/pdum8vb1g7vryxvmjyv6.jpg",
+  caption: "“If one day the speed kills me, don't cry. Because I was smiling.”",
+  location: "Las Vegas, Nevada"
+)
